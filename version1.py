33d661c99ec4c2cb255a45e6b38e73104077a1c5
d4rg4n <santibayona1@gmail.com>
2023-05-16 22:18:42 +0200

    modified:   version1.py

--- version1.py
+++ version1.py
@@ -1,5 +1,5 @@
-def saludar():
-    print("¡Hola desde functions.py!")
+def saludar(nombre):
+    print(f"¡Hola, {nombre}! ¡Bienvenido desde functions.py!")
 
-def despedir():
-    print("¡Hasta luego desde functions.py!")
+def despedir(nombre):
+    print(f"¡Hasta luego, {nombre}! ¡Nos vemos desde functions.py!")
